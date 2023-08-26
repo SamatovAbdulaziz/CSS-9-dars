54e3fc2e61c9bf983180fb7d17e3f981c638a285
aqdtjjksytjksfyksk <142991426+SamatovAbdulaziz@users.noreply.github.com>
2023-08-26 13:18:22 +0500

    CSS 9-dars
    26-guruh o'quvchisi

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap");
+let byFormGroup = document.getElementsByTagName("div").namedItem("form-group");
+
+byWrap.style.width = "1200px";
+byWrap.style.margin = "0 auto";
+
+byFormGroup.style.width = "500px";
+byFormGroup.style.margin = "0 auto";
+byFormGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+byFormGroup.style.padding = "1rem 1.2rem";
+byFormGroup.style.boxSizing = "border-box";
